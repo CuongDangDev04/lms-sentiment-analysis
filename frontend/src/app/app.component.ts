21a95668b3c318f41879636e478e1e65bc9baa8c
DaiNam159 <dainam15986@gmail.com>
2024-10-27 20:22:56 +0700

    Bug giao diện

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { HomeComponent } from './student/home/home.component';
 import { HeaderComponent } from './student/header/header.component';
 import { FooterComponent } from './student/footer/footer.component';
+import { AboutUsComponent } from './student/about-us/about-us.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, HeaderComponent, FooterComponent],
+  imports: [
+    RouterOutlet,
+    //RouterModule,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutUsComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './student/home/home.component';
+import { AboutUsComponent } from './student/about-us/about-us.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
     title: 'Home Page',
+  },
+  {
+    path: 'about-us',
+    component: AboutUsComponent,
+    title: 'About Us Page',
   },
 ];
