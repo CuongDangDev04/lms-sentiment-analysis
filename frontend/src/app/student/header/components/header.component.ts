3486deaa5ebd949ca87a2f87eb5b606480e86cf9
DaiNam159 <dainam15986@gmail.com>
2024-10-27 22:37:17 +0700

    update header v1

--- frontend/src/app/student/header/components/header.component.ts
+++ frontend/src/app/student/header/components/header.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  isHomePage: boolean = false;
+  isAboutPage: boolean = false;
+  isCoursesPage: boolean = false;
+  isContactPage: boolean = false;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(() => {
+      this.isHomePage = this.router.url === '/' || this.router.url === '/home';
+      this.isAboutPage = this.router.url === '/about-us';
+    });
+  }
+}
