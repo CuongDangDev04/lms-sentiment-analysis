55ff75f2f71da013b16ee5d5f44d2683c2c1eba3
BlueSkyDev04 <cuongdang.270920@gmail.com>
2024-11-10 22:37:33 +0700

    update ...

--- frontend/src/app/auth/auth.guard.ts
+++ frontend/src/app/auth/auth.guard.ts
@@ -11,11 +11,14 @@ export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-    const user = this.authService.getUser();
-    if (user && route.data['roles'] && route.data['roles'].includes(user.role)) {
+    //comment code để tạm thời bỏ tính năng phân quyền trong quá trình phát triển
+    //để tránh đỡ phiền bắt đăng nhập hoài, hehe :v 
+
+    // const user = this.authService.getUser();
+    // if (user && route.data['roles'] && route.data['roles'].includes(user.role)) {
       return true;
-    }
-    this.router.navigate(['login']);
-    return false;
+    // }
+    // this.router.navigate(['login']);
+    // return false;             
   }
 }
