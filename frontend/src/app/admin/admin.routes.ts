0633ba84eb43edcd68c497bc020d4666dd035955
BlueSkyDev04 <cuongdang.270920@gmail.com>
2024-11-10 23:47:45 +0700

    ...

--- frontend/src/app/admin/admin.routes.ts
+++ frontend/src/app/admin/admin.routes.ts
@@ -1,31 +1,3 @@
-// import { Routes } from '@angular/router';
-// import { AdminComponent } from './admin.component';
-// import { ManagerCoursesAdminComponent } from './components/manager-courses-admin/manager-courses-admin.component';
-// import { ManagerUsersAdminComponent } from './components/manager-users-admin/manager-users-admin.component';
-// import { DashboardAdminComponent } from './components/dashboard-admin/dashboard-admin.component';
-// import { FeedbackManagerAdminComponent } from './components/feedback-manager-admin/feedback-manager-admin.component';
-
-// export const adminRoutes: Routes = [
-//     {
-//         path: 'admin',
-//         component: AdminComponent,
-//         children: [
-            
-//             {
-//                 path:"quanlykhoahoc", component:ManagerCoursesAdminComponent            
-//             },
-//             {
-//                 path:"quanlynguoidung", component: ManagerUsersAdminComponent
-//             },
-//             {
-//                 path:'dashboard', component: DashboardAdminComponent
-//             },
-//             {
-//                 path:'feedbackmanger', component:FeedbackManagerAdminComponent
-//             }
-//         ]
-//     }
-// ];
 // src/app/admin/admin.routes.ts
 import { Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
--- frontend/src/app/student/student.routes.ts
+++ frontend/src/app/student/student.routes.ts
@@ -1,40 +1,23 @@
-// import { Routes } from '@angular/router';
-// import { StudentComponent } from './student.component';
-// import { HomeStudentComponent } from './components/home-student/home-student.component';
-// import { AboutStudentComponent } from './components/about-student/about-student.component';
-
-// export const studentRoutes: Routes = [
-//     {
-//         path: 'student',
-//         component: StudentComponent,
-//         children: [
-//             {
-//                 path:"", component: HomeStudentComponent
-//             },
-//             {
-//                 path:"about", component: AboutStudentComponent
-//             }
-//         ]
-//     }
-// ];
 // src/app/student/student.routes.ts
 import { Routes } from '@angular/router';
 import { StudentComponent } from './student.component';
 import { HomeStudentComponent } from './components/home-student/home-student.component';
 import { AboutStudentComponent } from './components/about-student/about-student.component';
-import { AuthGuard } from '../auth/auth.guard';
-import { Role } from '../models/roles';
 
 import { CourseStudentComponent } from './components/course-student/course-student.component';
 import { ContactStudentComponent } from './components/contact-student/contact-student.component';
 import { DashboardStudentComponent } from './components/dashboard-student/dashboard-student.component';
 import { DetailStudentComponent } from './components/detail-student/detail-student.component';
+import { AuthGuard } from '../auth/auth.guard';
+import { Role } from '../models/roles';
 
 export const studentRoutes: Routes = [
 
   {
     path: 'student',
     component: StudentComponent,
+    canActivate: [AuthGuard],
+    data: { roles: [Role.Student] },
     children: [
       {
         path: '',
