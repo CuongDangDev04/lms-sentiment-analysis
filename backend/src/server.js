65cdb23356cf1078f5548e0212eda62b0c93cdd3
BlueSkyDev04 <cuongdang.270920@gmail.com>
2024-11-15 13:46:42 +0700

    chuẩn bị merge code

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,14 +4,12 @@ const cors = require("cors");
 const sequelize = require("./config/db"); // Kết nối với cơ sở dữ liệu
 const authRoutes = require("./routes/auth"); // Routes cho đăng ký và đăng nhập
 require("dotenv").config(); // Tải biến môi trường từ .env
-const courseRoutes = require('./routes/course');
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
 app.use("/api/auth", authRoutes); // Đường dẫn cho auth
-app.use('/api', courseRoutes);
 
 const PORT = process.env.PORT || 3000;
 
